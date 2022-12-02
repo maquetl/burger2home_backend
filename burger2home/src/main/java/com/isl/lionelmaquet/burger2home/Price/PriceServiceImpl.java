5e4a60311aca25039d5aabe336a893ecb591dd03
Lionel Maquet <lionel.maquet@gmail.com>
2022-12-02 20:21:19 +0100

    adds get-put-post-delete endpoints for prices NOT TESTED

--- burger2home/src/main/java/com/isl/lionelmaquet/burger2home/Price/PriceServiceImpl.java
+++ burger2home/src/main/java/com/isl/lionelmaquet/burger2home/Price/PriceServiceImpl.java
@@ -16,4 +16,34 @@ public class PriceServiceImpl implements PriceService {
     public Optional<Price> getCurrentPriceByProductId(Integer productId) {
         return Optional.ofNullable(priceRepository.findCurrentPriceByProductId(productId));
     }
+
+    @Override
+    public List<Price> getAllPricesByProduct(Integer productIdentifier) {
+        return priceRepository.findByProductId(productIdentifier);
+    }
+
+    @Override
+    public List<Price> getAllPrices() {
+        return priceRepository.findAll();
+    }
+
+    @Override
+    public Optional<Price> getSinglePrice(Integer priceIdentifier) {
+        return priceRepository.findById(priceIdentifier);
+    }
+
+    @Override
+    public void createPrice(Price price) {
+        priceRepository.save(price);
+    }
+
+    @Override
+    public void modifyPrice(Price price) {
+        priceRepository.save(price);
+    }
+
+    @Override
+    public void deletePrice(Integer priceIdentifier) {
+        priceRepository.deleteById(priceIdentifier);
+    }
 }
