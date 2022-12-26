7b6e80806884489784ed89eefd2855d36d6109e2
lionel <lionel.maquet@gmail.com>
2022-12-26 18:29:49 +0100

    Updates cors config

--- burger2home/src/main/java/com/isl/lionelmaquet/burger2home/Security/CorsConfig.java
+++ burger2home/src/main/java/com/isl/lionelmaquet/burger2home/Security/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
+        registry.addMapping("/*****")
                 .allowedMethods("*")
                 .allowedOrigins("http://localhost:3000")
                 .allowedHeaders("*")
